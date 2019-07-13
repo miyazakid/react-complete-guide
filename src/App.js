74c50711591a6d5f3c0dbb49498221ae8bd36ea6
Fintan Maher <34825464+miyazakid@users.noreply.github.com>
2019-07-13 18:29:05 -0400

    Section 3 Lecture 42

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from 'react';
+import './App.css';
+import Person from './Person/Person';
+
+class App extends Component {
+  state = {
+    persons: [
+      { name: 'Fintan', age: 31 },
+      { name: 'Anielle', age: 27 },
+      { name: 'Craic Man', age: 45 }
+    ]
+  }
+
+  switchNameHandler = () => {
+    // console.log('Was Clicked');
+    // DON'T DO THIS: this.state.persons[0].name = 'Fontana';
+    this.setState({
+      persons: [
+        { name: 'Fontana', age: 31 },
+        { name: 'Anielle', age: 27 },
+        { name: 'Craic Man', age: 47 }
+      ]
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi, I'm a react App!</h1>
+        <p>This is really working!</p>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age} />
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age} />
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age} />
+      </div>
+    );
+  }
+}
+
+export default App;
